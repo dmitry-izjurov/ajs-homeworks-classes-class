0d38b261c9b271580f67630726bd2f162e94b55b
Dmitry <dmitry.izjurov@yandex.ru>
2022-04-10 18:19:22 +0300

    Добавлены условия проверки name и type

--- src/js/basic.js
+++ src/js/basic.js
@@ -2,5 +2,15 @@ export default class Character {
   constructor(name, type) {
     this.name = name;
     this.type = type;
+
+    if (typeof this.name !== 'string' || this.name.length < 2 || this.name.length > 10) {
+      throw new Error('Некорректное значение "name"');
+    }
+
+    const unit = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].find((a) => a === this.type);
+
+    if (!unit || typeof this.type !== 'string') {
+      throw new Error('Некорректное значение "type"');
+    }
   }
 }
